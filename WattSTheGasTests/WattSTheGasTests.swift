d323438b447ee2d666a80a1df288c558d916638b
Jose Marcos RF <10252518+josemarcosrf@users.noreply.github.com>
2026-03-21 10:16:47 +0100

    Initial Commit

---
+++ WattSTheGasTests/WattSTheGasTests.swift
@@ -0,0 +1,17 @@
+//
+//  WattSTheGasTests.swift
+//  WattSTheGasTests
+//
+//  Created by Jose Marcos RF on 21/3/26.
+//
+
+import Testing
+@testable import WattSTheGas
+
+struct WattSTheGasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
